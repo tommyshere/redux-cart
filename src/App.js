be8dddf31264dd0a031265c1d27621986281bac5
Tommy <tommyshere@gmail.com>
2021-04-14 13:04:52 -0700

    notification

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -5,6 +6,15 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  // gets the entire state
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-http-cc127-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
